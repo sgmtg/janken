2436230942e0c185a1b128b2c4047523698d3993
Gen Sugimoto <shanbenyan3@gmail.com>
2023-02-19 15:52:22 +0900

    [add] no style

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+$handTypeNames = ['グー', 'チョキ', 'パー'];
+$handImgFile = ['gu', 'choki', 'pa'];
+$resultsText = ['あいこ', 'あなたの負けです…', 'あなたの勝ちです!'];
+if(isset($_POST['selectedNum'])){
+    $userHandNum = (int)$_POST['selectedNum'];
+    $pcHandNum = rand(0, count($handTypeNames)-1);
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +19,23 @@
     <title>じゃんけん</title>
 </head>
 <body>
+    <form method='post'>
+        <?php for($num=0; $num<count($handTypeNames); $num++): ?>
+            <?php $checked = isset($userHandNum) && $userHandNum===$num ? 'checked':''; //イコール三つは型まで完全に一致?>
+            <input type='radio' name="selectedNum" value="<?=$num ?>" <?=$checked?> > <?=$handTypeNames[$num]?> <br>
+        <?php endfor ?>
+        <button type="submit">勝負！</button>
+    </form>
+
+
+<?php if(isset($_POST['selectedNum'])):?>
+  <div>
+    <img src="img/janken_<?=$handImgFile[$userHandNum]?>.png">
+    <img src="img/janken_<?=$handImgFile[$pcHandNum]?>.png">
+  </div>
+  <p><?=$resultsText[($userHandNum + 3 -$pcHandNum) % 3]?></p>
+<?php endif?>
+
     
 </body>
 </html>
